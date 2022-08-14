438358627a8f05fe510a3fc2fdd5de5fab1d3b01
Deviate-DV8 <88654163+Danvictorgithub@users.noreply.github.com>
2022-08-14 21:16:10 +0800

    Added Product Component

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,14 +1,58 @@
 import './Products.css';
-import xboxX from '../xboxX.png';
 import steamDeck from '../steam_deck_03.jpg';
 import React, {useState, useEffect} from 'react';
+
+import xboxX from '../xboxX.png';
+import xboxS from '../xboxS.png';
+import ps5 from '../ps5.png';
+import nswitch from '../switch.png';
+import steamdeck from '../steamDeck.png';
+import legion7 from '../legion7.png';
+import nitro5 from '../nitro5.png';
+import victus16 from '../victus16.png';
+
 function Products() {
     const [products,addProduct] = useState([
         {
             name:'Xbox Series X',
             img:xboxX,
             price:'499',
-        }
+        },
+        {
+            name:'Xbox Series S',
+            img:xboxS,
+            price:'699',
+        },
+        {
+            name:'PS5',
+            img:ps5,
+            price:'699',
+        },
+        {
+            name:'Nintendo Switch',
+            img:nswitch,
+            price:'399',
+        },
+        {
+            name:'Steam Deck',
+            img:steamdeck,
+            price:'600',
+        },
+        {
+            name:'Lenovo Legion 7',
+            img:legion7,
+            price:'2000',
+        },
+        {
+            name:'Acer Nitro 5',
+            img:nitro5,
+            price:'1100',
+        },
+        {
+            name:'HP Victus 16',
+            img:victus16,
+            price:'1099',
+        },
     ]);
     return(
         <div className='Products'>
@@ -32,15 +76,11 @@ function Products() {
                             <div className='card'>
                                 <img src={product.img}/>
                                 <p className='productName'>{product.name}</p>
-                                <p className='price'>{product.price}</p>
+                                <p className='price'>{product.price} $</p>
                                 <button type='button' className='productButton'>Buy</button>
                             </div>
                         )
                     })}
-                    <div className='card'></div>
-                    <div className='card'></div>
-                    <div className='card'></div>
-                    <div className='card'></div>
                 </div>
             </div>
         </div>
