85abeeda101175cbd20a5dab06bbf55befe1de7b
Deviate-DV8 <88654163+Danvictorgithub@users.noreply.github.com>
2023-01-11 10:33:16 +0800

    Fixed deployment issues

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
 ;  }
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename="/shopping-cart">
         <Nav cartNum={cartItems.length}/>
         <Routes>
           <Route path='/' element={<Home/>}></Route>
