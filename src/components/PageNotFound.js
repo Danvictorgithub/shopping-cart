7960302b6e0e9dc2d1dda7fa19331049923f188c
Deviate-DV8 <88654163+Danvictorgithub@users.noreply.github.com>
2023-01-11 10:31:18 +0800

    Updates

--- src/components/PageNotFound.js
+++
@@ -1,9 +0,0 @@
-import './PageNotFound.css';
-function PageNotFound() {
-    return(
-        <div className='PNF'>
-            <h2>Page Not Found</h2>
-        </div>
-    )
-}
-export default PageNotFound;
